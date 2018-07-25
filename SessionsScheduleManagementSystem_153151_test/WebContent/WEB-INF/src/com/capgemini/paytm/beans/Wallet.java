8e13a8c653964635ea0000edce91ecff31d9b294
nagasravankumar <40980856+nagasravankumar@users.noreply.github.com>
2018-07-25 11:24:17 +0530

    Add files via upload

---
+++ SessionsScheduleManagementSystem_153151_test/WebContent/WEB-INF/src/com/capgemini/paytm/beans/Wallet.java
@@ -0,0 +1,33 @@
+package com.capgemini.paytm.beans;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Embeddable;
+@Embeddable
+public class Wallet {
+
+	private BigDecimal balance;
+
+	public Wallet(BigDecimal balance) {
+		super();
+		this.balance = balance;
+	}
+
+	public Wallet() {
+		super();
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return  balance + "\n";
+	}
+	
+}
